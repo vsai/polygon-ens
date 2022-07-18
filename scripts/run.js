3fca560a0217532c18a706a59534f84c678d6552
vsai <vishalsai@gmail.com>
2022-07-18 18:06:55 +0800

    Domains.sol: make the contract inherit ERC721 (NFT-able)
    Make the contract support NFTs with an SVG image too

--- scripts/run.js
+++ scripts/run.js
@@ -1,16 +1,20 @@
 const main = async () => {
   const [owner, randomPerson] = await hre.ethers.getSigners();
+  const tld = "ysm";
+  const domainName = "helloworld";
+
   const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-  const domainContract = await domainContractFactory.deploy("ysm"); // tld = ysm
+  const domainContract = await domainContractFactory.deploy(tld);
   await domainContract.deployed();
+
   console.log("Contract deployed to:", domainContract.address);
   console.log("Contract deployed by:", owner.address);
 
-  const txn = await domainContract.register("helloworld", { value: hre.ethers.utils.parseEther("0.1") });
+  const txn = await domainContract.register(domainName, { value: hre.ethers.utils.parseEther("0.1") });
   await txn.wait();
 
-  const domainOwner = await domainContract.getAddress("helloworld");
-  console.log("Owner of domain: ", domainOwner);
+  const domainOwner = await domainContract.getAddress(domainName);
+  console.log(`Owner of domain ${domainName}:`, domainOwner);
 
   const balance = await hre.ethers.provider.getBalance(domainContract.address);
   console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
