fadee51e2dd338a84f406c8c8033feaeb12c4c61
vsai <vishalsai@gmail.com>
2022-07-22 19:21:23 +0800

    Domains.sol: add ability to withdraw from the contract for creator

--- scripts/run.js
+++ scripts/run.js
@@ -10,14 +10,33 @@ const main = async () => {
   console.log("Contract deployed to:", domainContract.address);
   console.log("Contract deployed by:", owner.address);
 
-  const txn = await domainContract.register(domainName, { value: hre.ethers.utils.parseEther("0.1") });
+  let txn = await domainContract.register(domainName, { value: hre.ethers.utils.parseEther("1234") });
   await txn.wait();
 
-  const domainOwner = await domainContract.getAddress(domainName);
-  console.log(`Owner of domain ${domainName}:`, domainOwner);
+  // const domainOwner = await domainContract.getAddress(domainName);
+  // console.log(`Owner of domain ${domainName}:`, domainOwner);
 
   const balance = await hre.ethers.provider.getBalance(domainContract.address);
   console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
+
+  try {
+    txn = await domainContract.connect(randomPerson).withdraw();
+    await txn.wait();
+  } catch(error) {
+    console.log("Could not rob contract");
+  }
+
+  let ownerBalance = await hre.ethers.provider.getBalance(owner.address);
+  console.log("Balance of owner before withdrawal balance:", hre.ethers.utils.formatEther(ownerBalance));
+
+  txn = await domainContract.connect(owner).withdraw();
+  await txn.wait();
+
+  const contractBalance = await hre.ethers.provider.getBalance(domainContract.address);
+  ownerBalance = await hre.ethers.provider.getBalance(owner.address);
+
+  console.log("Contract balance after withdrawal:", hre.ethers.utils.formatEther(contractBalance));
+  console.log("Balance of owner after withdrawal:", hre.ethers.utils.formatEther(ownerBalance));
 
   // txn = await domainContract.connect(randomPerson).setRecord("ysm", "I want the domain");
   // await txn.wait();
